68030d38ed1e9723104aa06cf39d967939295b3b
A1aaSayed <alaa02.sayed02@gmail.com>
2023-10-15 21:21:42 +0300

    edit printf.c

--- _printf.c
+++ _printf.c
@@ -46,7 +46,7 @@ int _printf(const char *format, ...)
 			}
 			else if (*format == '%')
 			{
-				write(1, "%", 1);
+				write(1, "%%", 1);
 				count++;
 			}
 			else if (*format == 'd' || *format == 'i')
