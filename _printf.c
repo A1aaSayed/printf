351f78c5790f814b8dcfcfde097fdcffe50eba6e
A1aaSayed <alaa02.sayed02@gmail.com>
2023-10-16 17:44:08 +0300

    edit printf.c file

--- _printf.c
+++ _printf.c
@@ -14,54 +14,26 @@ int _printf(const char *format, ...)
 	va_start(list, format);
 	if (format == NULL)
 		return (-1);
-	while (*format)
-	{
-		if (*format != '%')
-		{
-			write(1, format, 1);
-			count++;
-		}
-		else
-		{
-			format++;
-			if (*format == '\0')
-				break;
-			if (*format == 'c')
-			{
-				char ch = va_arg(list, int);
-
-				write(1, &ch, 1);
-				count++;
-			}
-			else if (*format == 's')
-			{
-				char *str = va_arg(list, char*);
-
-				while (*str)
-				{
-					write(1, str, 1);
-					str++;
-					count++;
-				}
-			}
-			else if (*format == '%')
-			{
-				write(1, "%%", 1);
-				count++;
-			}
-			else if (*format == 'd' || *format == 'i')
-			{
-				value = va_arg(list, int);
-				while (value > 0)
-				{
-					num = value % 10;
-					digit = '0' + num;
-					write(1, &digit, 1);
-					value /= 10;
-				}
-			}
-		}
-		format++;
+	while (*format != '\0') {
+    if (*format == '%') {
+      switch (*++format) {
+        case 'c':
+          count += putchar(va_arg(args, int));
+          break;
+        case 's':
+          count += fputs(va_arg(args, char *), stdout);
+          break;
+        case '%':
+          count += putchar('%');
+          break;
+        default:
+          // Ignore unknown format specifiers.
+          break;
+      }
+    } else {
+      count += putchar(*format);
+    }
+    format++;
 	}
 	va_end(list);
 	return (count);
