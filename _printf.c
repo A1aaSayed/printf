3e222c191ba85c4d2cd013106940b8317d08750d
A1aaSayed <alaa02.sayed02@gmail.com>
2023-10-16 16:49:15 +0300

    edit printf.c file

--- _printf.c
+++ _printf.c
@@ -9,7 +9,7 @@
 int _printf(const char *format, ...)
 {
 	va_list list;
-	int count = 0, value, num, digit;
+	int count = 0;
 
 	va_start(list, format);
 	if (format == NULL)
@@ -17,52 +17,9 @@ int _printf(const char *format, ...)
 	while (*format)
 	{
 		if (*format != '%')
-		{
 			write(1, format, 1);
-			count++;
-		}
-		else
-		{
-			format++;
-			if (*format == '\0')
-				break;
-			if (*format == 'c')
-			{
-				char ch = va_arg(list, int);
-
-				write(1, &ch, 1);
-				count++;
-			}
-			else if (*format == 's')
-			{
-				char *str = va_arg(list, char*);
-
-				while (*str)
-				{
-					write(1, str, 1);
-					str++;
-					count++;
-				}
-			}
-			else if (*format == '%')
-			{
-				write(1, "%%", 1);
-				count++;
-			}
-			else if (*format == 'd' || *format == 'i')
-			{
-				value = va_arg(list, int);
-				while (value > 0)
-				{
-					num = value % 10;
-					digit = '0' + num;
-					write(1, &digit, 1);
-					value /= 10;
-				}
-			}
-		}
 		format++;
 	}
-	va_end(list);
 	return (count);
+	va_end(list);
 }
