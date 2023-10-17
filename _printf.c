1f6a82586d673296cc114bf9733cf3ae4c7bb115
Y-admen <yassminash305@gmail.com>
2023-10-17 11:46:28 -0700

    test

--- _printf.c
+++ _printf.c
@@ -3,31 +3,31 @@
 
 int _printf(const char *format, ...)
 {
-        va_list list;
-        int len = 0, i;
-        va_start(list, format);
+	va_list list;
+	int len = 0, i;
 
-        if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-                return (-1);
-        while (*format)
-        {
-        if (format[i] == '%')
-        {
-            if (format[i + 1] == 'c')
-                len += p_char(list);
-            else if (format[i + 1] == 's')
-                len += p_string(list);
-            else if (format[i + 1] == '%')
-                len += p_percent();
-            else if (format[i + 1] == 'd' || format[i + 1] == 'i')
-                len = p_int(list);
-        }
-        else
-            len += _putchar(format[i]);
-        i++;
+	va_start(list, format);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+	while (*format)
+	{
+	if (format[i] == '%')
+	{
+		if (format[i + 1] == 'c')
+			len += p_char(list);
+		else if (format[i + 1] == 's')
+			len += p_string(list);
+		else if (format[i + 1] == '%')
+			len += p_percent();
+		else if (format[i + 1] == 'd' || format[i + 1] == 'i')
+			len = p_int(list);
+	}
+	else
+		len += _putchar(format[i]);
+	i++;
 
-        }
-        va_end(list);
-        return (len);
+	}
+	va_end(list);
+	return (len);
 }
 
--- func.c
+++ func.c
@@ -4,89 +4,89 @@
 
 int p_char(va_list list)
 {
-        char s = va_arg(list, int);
-        return (_putchar(s));
+	char s = va_arg(list, int);
+
+	return (_putchar(s));
 }
 
-int _strlen (char *p)
+int _strlen(char *p)
 {
-        int len = 0;
+	int len = 0;
 
-        while(*p++)
-                len++;
-        return (len);
+	while (*p++)
+		len++;
+	return (len);
 }
 
 /***/
 
 int p_percent(void)
 {
-        return (_putchar('%'));
+	return (_putchar('%'));
 }
 
 /***/
 
 int p_string(va_list list)
 {
-        int i, len;
-        char *p = va_arg(list, char *);
+	int i, len;
+	char *p = va_arg(list, char *);
 
-        if (!p)
-        {
-                p = "(null)";
-                len = _strlen(p);
-                for (i = 0; i < len; i++)
-                        _putchar(p[i]);
-                return (len);
-        }
-        if (p[0] == '\0')
-                return (-1);
+	if (!p)
+	{
+		p = "(null)";
+		len = _strlen(p);
+		for (i = 0; i < len; i++)
+			_putchar(p[i]);
+		return (len);
+	}
+	if (p[0] == '\0')
+	return (-1);
+	for (i = 0; p[i] != '\0'; i++)
+		len += _putchar(p[i]);
 
-        for (i = 0; p[i] != '\0'; i++)
-                len += _putchar(p[i]);
-
-        return (len);
+	return (len);
 }
 
 /***/
 
 int p_int(va_list args)
 {
-  int n = va_arg(args, int);
-  int num, last = n % 10, digit, exp = 1;
-  int i = 1;
+	int n = va_arg(args, int);
+	int num, last = n % 10, digit, exp = 1;
+	int i = 1;
 
-  n = n / 10;
-  num = n;
+	n = n / 10;
+	num = n;
 
-  if (last < 0)
-  {
-    _putchar('-');
-    num = -num;
-    n = -n;
-    last = -last;
-    i++;
-  }
-  if (num > 0)
-  {
-    while (num / 10 != 0)
-    {
-      exp = exp * 10;
-      num = num / 10;
-    }
-    num = n;
-    while (exp > 0)
-    {
-        digit = num / exp;
-        _putchar(digit + '0');
-        num = num - (digit * exp);
-      exp = exp / 10;
-      i++;
-    }
-  }
-  _putchar(last + '0');
+	if (last < 0)
+	{
+		_putchar('-');
+		num = -num;
+		n = -n;
+		last = -last;
+		i++;
+	}
+	if (num > 0)
+	{
+	while (num / 10 != 0)
+	{
+		exp = exp * 10;
+		num = num / 10;
+	}
+	num = n;
+	while (exp > 0)
+	{
+		digit = num / exp;
+		_putchar(digit + '0');
+		num = num - (digit * exp);
+		exp = exp / 10;
+		i++;
+	}
+	}
+	_putchar(last + '0');
 
-  return (i);
+	return (i);
 }
 
 
